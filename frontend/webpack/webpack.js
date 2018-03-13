a0cbda8135ebb924706c4bde9df12a5a8fa9fb95
SAPIENT\ksriv7 <ksrivastav9@sapient.com>
2018-03-13 16:46:02 +0530

    initial commit

---
+++ frontend/webpack/webpack.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
+const dev = require('webpack-dev-server');
+const mode = process.argv[2];
+
+console.log(path.resolve(__dirname, '../src'))
+
+// for dev server
+
+
