c9453e6b884a61d99fa9bcfcf77aae1a852d909f
ketansrivastav <ketan.srivastav@gmail.com>
2018-03-17 11:45:57 +0530

    scaffolding - webpack scripts - backend

---
+++ bin/config/paths.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports =   {
+    frontend_src: path.resolve('.','frontend/src'),
+    www: path.resolve('.',"www"),
+    dev_www: path.resolve('.','frontend/dev-www'),
+    express_views: path.resolve('.','backend/views')
+}
---
+++ bin/config/webpack.prod.config.js
@@ -0,0 +1,57 @@
+
+const {frontend_src,www} = require('./paths.js')
+const path = require('path');
+
+module.exports = {
+    
+        // Configuration Object  
+    
+           
+            context: path.resolve('.'),
+            entry: {
+                app:[frontend_src + '/app.js']
+            },
+    
+            output: {
+    
+              path: dev_www + '/',
+              publicPath: dev_www + '/',
+              filename: "bundle.js"
+    
+            },
+    
+            module:{
+    
+              rules:[
+    
+                //   {
+    
+                //       test:/\.css$/,
+    
+                //       use:['style-loader','css-loader']
+    
+                //   }
+    
+                
+                    {
+                      test: /\.js$/,
+                      exclude: /node_modules/,
+                      use: {
+                        loader: "babel-loader",
+                     
+                      query : {
+                        presets: ["env", "react"],
+                        
+                        
+                      }
+                    }, 
+                    }
+                  
+    
+             ],
+    
+          },
+    
+                
+    
+      }
